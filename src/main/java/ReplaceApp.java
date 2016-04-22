9b809546a25d817d620bb596b9a9e573c613cf9a
blessb15 <blakebless7@gmail.com>
2016-04-22 11:16:28 -0700

    running with spark. html elements added aswell as css.

--- src/main/java/ReplaceApp.java
+++ src/main/java/ReplaceApp.java
@@ -1,7 +1,29 @@
-
+import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class ReplaceApp {
   public static void main(String[] args) {
+    staticFileLocation("/public");
+
+    get("/",(request,response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
+
+    get("/detector",(request,response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String userInput = request.queryParams("userinput").toLowerCase();
+      ReplaceApp newApp = new ReplaceApp();
+      String newReplace = newApp.replaceIt(userInput);
+      model.put("newReplace", newReplace);
+      model.put("template", "templates/detector.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
 
 
   }
